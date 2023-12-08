404f07f2c3dbc2b571007f3ceebd04931261db42
Nikita <kofman.niki@gmail.com>
2023-12-09 00:34:19 +0100

    m

--- functions/deleteUserFunction/index.js
+++ functions/deleteUserFunction/index.js
@@ -6,10 +6,10 @@ module.exports = async function ({ req, res, log, error }) {
   const client = new Client();
   client
     .setEndpoint("https://cloud.appwrite.io/v1")
-    .setProject(
+    .setProject("656b02880083dd7e9d6c")
+    .setKey(
       "7ee6413de7c9224361723dc689753a5152b5e33895526042ced810a574e0452ba8c757839a54ab5a65d28a0c92854b7432a4615ef65a03b10d707f9fcfcaa50cca9b799fd65aa20105c14b5a1bcce0a2679442cc48cc70cdc9bbcc15b8a1933e849cc06646ffda33f2a51e56f87b052fd6abd1e936d2bde6f9d59f6083c0e115"
-    )
-    .setKey(process.env.APPWRITE_API_KEY);
+    );
 
   const users = new Users(client);
 
