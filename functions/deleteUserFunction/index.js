e79e7f27856a8d162b9a8c0361c369e94d2d679f
Nikita <kofman.niki@gmail.com>
2023-12-08 22:49:10 +0100

    m

--- functions/deleteUserFunction/index.js
+++ functions/deleteUserFunction/index.js
@@ -2,7 +2,7 @@ const { Client, Account, Users } = require("node-appwrite");
 
 module.exports = async function ({ req, res, log, error }) {
   log(`Request Body: ${JSON.stringify(req.body)}`);
-  log(req.body);
+
   // Initialize the Appwrite client
   const client = new Client();
   const account = new Account(client);
@@ -14,8 +14,9 @@ module.exports = async function ({ req, res, log, error }) {
     .setKey(process.env.APPWRITE_API_KEY);
 
   try {
-    // Access userId directly from the request body
-    const userId = req.body.userId;
+    // Parse the JSON string in req.body
+    const parsedBody = JSON.parse(req.body);
+    const userId = parsedBody.userId;
     log(`Received userId: ${userId}`);
 
     const result = await users.delete(userId);
