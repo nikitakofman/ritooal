4cbd15c25c51edf71c453b0d9110732ddaf0a48e
Nikita <kofman.niki@gmail.com>
2023-12-08 22:51:39 +0100

    bruh

--- functions/deleteUserFunction/index.js
+++ functions/deleteUserFunction/index.js
@@ -1,6 +1,7 @@
 const { Client, Account, Users } = require("node-appwrite");
 
 module.exports = async function ({ req, res, log, error }) {
+  log(`Raw request body: ${req.body}`);
   log(`Request Body: ${JSON.stringify(req.body)}`);
 
   // Initialize the Appwrite client
--- pages/api/deleteUser.ts
+++ pages/api/deleteUser.ts
@@ -12,12 +12,15 @@ export default async function handler(req, res) {
   }
 
   try {
+    const requestBody = JSON.stringify({ userId });
+    console.log("Request body to Appwrite Function:", requestBody);
+
     const response = await fetch(
       "https://cloud.appwrite.io/v1/functions/65734ae49c41205b0552/executions",
       {
         method: "POST",
         headers: headers,
-        body: JSON.stringify({ userId }),
+        body: requestBody,
       }
     );
 
