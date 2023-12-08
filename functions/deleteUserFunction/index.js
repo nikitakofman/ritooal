2451e60dd209381454bc032438deeea74aed53bc
Nikita <kofman.niki@gmail.com>
2023-12-08 21:34:53 +0100

    m

--- functions/deleteUserFunction/index.js
+++ functions/deleteUserFunction/index.js
@@ -1,39 +1,32 @@
 const { Client, Account, Users } = require("node-appwrite");
 
-module.exports = async function (request, context) {
-  // Use context.log for standard logs
-
+module.exports = async function (request, { log, error }) {
   // Initialize the Appwrite client
   const client = new Client();
 
   client
     .setEndpoint("https://cloud.appwrite.io/v1")
-
     .setProject(process.env.APPWRITE_PROJECT_ID)
     .setKey(process.env.APPWRITE_API_KEY); // Your Appwrite API key with permissions
 
   const users = new Users(client);
 
-  log(`Attempting to delete user with ID: ${userId}`);
-
   try {
     const userId = request.payload.userId;
-
     log(`Attempting to delete user with ID: ${userId}`);
 
     let result = await users.delete(userId);
-
     log(`Deletion result: ${JSON.stringify(result)}`);
 
     return {
       message: "User deleted successfully",
       result: result,
     };
-  } catch (error) {
-    // Use context.error for error logs
+  } catch (err) {
+    error(`Failed to delete the user: ${err.message}`);
     return {
       message: "Failed to delete the user",
-      error: error.message,
+      error: err.message,
     };
   }
 };
