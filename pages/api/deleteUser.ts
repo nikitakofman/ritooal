fb3f227eaf5c8600fe5428d1561e3d6be55880be
Nikita <kofman.niki@gmail.com>
2023-12-08 19:09:44 +0100

    settings delete

--- pages/api/deleteUser.ts
+++ pages/api/deleteUser.ts
@@ -1,14 +1,20 @@
 export default async function handler(req, res) {
   console.log("hello");
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  if (process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID) {
+    headers.append(
+      "X-Appwrite-Project",
+      process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID
+    );
+  }
+  // Include your Appwrite API key here if necessary
+
   const response = await fetch(
     "https://cloud.appwrite.io/v1/functions/65734ae49c41205b0552/executions",
     {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Appwrite-Project": process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
-        // Include your Appwrite API key here if necessary
-      },
+      headers: headers,
       body: JSON.stringify(req.body),
     }
   );
