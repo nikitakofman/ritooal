125c982af442a272eae992b4142525857d91d9f1
Nikita <kofman.niki@gmail.com>
2023-12-08 22:26:47 +0100

    m

--- functions/deleteUserFunction/index.js
+++ functions/deleteUserFunction/index.js
@@ -1,30 +1,29 @@
 const { Client, Account, Users } = require("node-appwrite");
 
 module.exports = async function ({ req, res, log, error }) {
-  // Initialize the Appwrite client
   const client = new Client();
-
   client
     .setEndpoint("https://cloud.appwrite.io/v1")
     .setProject(process.env.APPWRITE_PROJECT_ID)
-    .setKey(process.env.APPWRITE_API_KEY); // Your Appwrite API key with permissions
+    .setKey(process.env.APPWRITE_API_KEY);
 
   const users = new Users(client);
 
   try {
-    const userId = JSON.parse(req.body).userId;
-    log(req.body);
-    log(`Attempting to delete user with ID: ${userId}`);
+    const payload = JSON.parse(req.body);
+    const userId = payload.userId;
+
+    log(`Received request to delete user ID: ${userId}`);
 
     const result = await users.delete(userId);
-    log(`Deletion result: ${JSON.stringify(result)}`);
+    log(`User deleted successfully: ${JSON.stringify(result)}`);
 
     return {
       message: "User deleted successfully",
       result: result,
     };
   } catch (err) {
-    // error(Failed to delete the user: ${err.message});
+    error(`Failed to delete the user: ${err.message}`);
     return {
       message: "Failed to delete the user",
       error: err.message,
--- pages/api/deleteUser.ts
+++ pages/api/deleteUser.ts
@@ -1,25 +1,32 @@
 export default async function handler(req, res) {
-  console.log("hello");
-  const headers = new Headers();
-  headers.append("Content-Type", "application/json");
-  if (process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID) {
-    headers.append(
-      "X-Appwrite-Project",
-      process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID
+  const headers = {
+    "Content-Type": "application/json",
+    "X-Appwrite-Project": process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
+    "X-Appwrite-Key": process.env.APPWRITE_API_KEY, // Uncomment and use if API key is required
+  };
+
+  console.log(req.body);
+  try {
+    const response = await fetch(
+      "https://cloud.appwrite.io/v1/functions/65734ae49c41205b0552/executions",
+      {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(req.body),
+      }
     );
-  }
-  // Include your Appwrite API key here if necessary
 
-  const response = await fetch(
-    "https://cloud.appwrite.io/v1/functions/65734ae49c41205b0552/executions",
-    {
-      method: "POST",
-      headers: headers,
-      body: JSON.stringify(req.body),
+    console.log("woo", response);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-  );
 
-  const data = await response.json();
-  res.status(response.status).json(data);
-  console.log(data);
+    const data = await response.json();
+    console.log("Response data:", data);
+    res.status(response.status).json(data);
+  } catch (error) {
+    console.error("Error making request to Appwrite:", error);
+    res.status(500).json({ error: error.message });
+  }
 }
