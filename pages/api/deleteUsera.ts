2ec43ba77f83caa18e8534edcbc4467f2215e3a9
Nikita <kofman.niki@gmail.com>
2023-12-09 12:35:32 +0100

    m

--- pages/api/deleteUsera.ts
+++ pages/api/deleteUsera.ts
@@ -19,14 +19,14 @@ export default async function handler(
     const client: any = new Client();
     const functions = new Functions(client);
 
-    const functionId: any = process.env.NEXT_PUBLIC_FUNCTION_ID;
+    // const functionId: any = process.env.NEXT_PUBLIC_FUNCTION_ID;
 
     client
       .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
       .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID); // Your project ID
 
     const execution = await functions.createExecution(
-      "65734ae49c41205b0552",
+      process.env.NEXT_PUBLIC_FUNCTION_ID!,
       JSON.stringify({ userId })
     );
 
